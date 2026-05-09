48d4e826eb2375e52b4b328fad8d99306d1c3560
Joshua Sajeev <joshuasajeevnv@gmail.com>
2026-05-09 11:07:35 +0530

    feat(ui): add mockup dashboard

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	r.Get("/", homeHandler)
+
+	log.Println("Server running on :3000")
+
+	log.Fatal(http.ListenAndServe(":3000", r))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := map[string]string{
+		"Name": "Joshua",
+	}
+
+	t, err := template.ParseFiles("./templates/index.html")
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Template error", http.StatusInternalServerError)
+		return
+	}
+
+	err = t.Execute(w, ctx)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Execution error", http.StatusInternalServerError)
+	}
 }
