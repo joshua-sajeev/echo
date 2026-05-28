a87be367fe88933c43bb842ad08056532d28eaf6
Joshua Sajeev <joshuasajeevnv@gmail.com>
2026-05-28 20:49:52 +0530

    refactor(db): make account name unique

--- backend/migrations/001_create_accounts_table.sql
+++ backend/migrations/001_create_accounts_table.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 CREATE TABLE IF NOT EXISTS accounts (
     id BIGSERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
+    name TEXT NOT NULL UNIQUE,
     is_archived BOOLEAN NOT NULL DEFAULT false,
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
